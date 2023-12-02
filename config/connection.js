b7ce0b084cc3790000c4e27521841337f85b0614
viaheather <viaheatherr@gmail.com>
2023-12-02 09:47:03 -0800

    pushing just in case

--- config/connection.js
+++ config/connection.js
@@ -1,7 +1,8 @@
-const { connect, connection } = require("mongoose");
+const mongoose = require("mongoose");
 
-process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/socialDB";
+mongoose.connect(
+  process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/socialDB"
+);
+mongoose.set("debug", true);
 
-connect(connectionString);
-
-module.exports = connection;
+module.exports = mongoose.connection;
