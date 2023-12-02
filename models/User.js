b4c41e302051b4ab6b0df041a0798c886451bd17
viaheather <viaheatherr@gmail.com>
2023-12-02 10:36:07 -0800

    updates user model

--- models/User.js
+++ models/User.js
@@ -1,35 +1,47 @@
-const { Schema } = require("mongoose");
+const { Schema, model } = require("mongoose");
 
-const userSchema = new Schema({
-  username: {
-    type: String,
-    unique: true,
-    required: true,
-    trim: true,
-  },
-  email: {
-    type: String,
-    required: true,
-    unique: true,
-    validate: [validateEmail, "Please fill a valid email address"],
-    match: [
-      /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
-      "Please fill a valid email address",
+function validateEmail(email) {
+  return /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email);
+}
+
+const userSchema = new Schema(
+  {
+    username: {
+      type: String,
+      unique: true,
+      required: true,
+      trim: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      validate: [validateEmail, "Please fill a valid email address"],
+      match: [
+        /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
+        "Please fill a valid email address",
+      ],
+    },
+    thoughts: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "Thoughts",
+      },
+    ],
+    friends: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "User",
+      },
     ],
   },
-  thoughts: {
-    type: Schema.Types.ObjectId,
-    ref: "Thoughts",
-  },
-  friends: {
-    type: Schema.Types.ObjectId,
-    ref: "User",
-  },
-  toJSON: {
-    virtuals: true,
-  },
-  id: false,
-});
+  {
+    toJSON: {
+      virtuals: true,
+    },
+    id: false,
+  }
+);
 
 userSchema.virtual("friendCount").get(function () {
   return this.friends.length;
--- utils/seed.js
+++ utils/seed.js
@@ -20,12 +20,9 @@ connection.once("open", async () => {
   if (userCheck.length) {
     await connection.dropCollection("user");
   }
-  // Create empty array to hold the users
   const users = [];
 
-  // Loop 20 times -- add students to the students array
   for (let i = 0; i < 20; i++) {
-    // Get some random assignment objects using a helper function that we imported from ./data
     const thoughts = getRandomThoughts(20);
 
     const fullName = getRandomName();
@@ -39,10 +36,8 @@ connection.once("open", async () => {
     });
   }
 
-  // Add students to the collection and await the results
   await User.collection.insertMany(users);
 
-  // Log out the seed data to indicate what should appear in the database
   console.table(users);
   console.info("Seeding complete! 🌱");
   process.exit(0);
